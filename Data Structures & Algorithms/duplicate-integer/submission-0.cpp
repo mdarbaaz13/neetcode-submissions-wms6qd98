24b12366c94133a751ee237088940c1f61f5f5dc
Md Arbaaz <mdarbaaz020@gmail.com>
2026-04-15 01:24:08 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+                }
+            }
+        if(count>0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+};
---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+                }
+            }
+        if(count>0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+};

4a0420ed89f4b4bd5b3d8b1e5a9bc0cadd6ef18e
Md Arbaaz <mdarbaaz020@gmail.com>
2026-06-06 19:37:49 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    bool isAlphaNumeric(char ch) {
+        if ((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')) {
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (!isAlphaNumeric(s[i])) {
+                i++;
+                continue;
+            }
+            if (!isAlphaNumeric(s[j])) {
+                j--;
+                continue;
+            }
+            if (tolower(s[i]) != tolower(s[j])) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    };
+};
